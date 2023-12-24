db3a492e39cce83daebf4ebb86904a3bcf0933d0
Hugos68 <hugokorteapple@gmail.com>
2023-12-24 17:28:05 +0100

    relocated routes

--- src/lib/hooks/lang.ts
+++ src/lib/hooks/lang.ts
@@ -2,12 +2,18 @@ import { sourceLanguageTag } from '$paraglide/runtime';
 import { type Handle } from '@sveltejs/kit';
 
 export const lang: Handle = async ({ event, resolve }) => {
-	const lang = event.params.lang ?? sourceLanguageTag;
+	const preferredLanguage = event.cookies.get('preferred-lang');
+	const acceptLanguage = event.request.headers.get('accept-language');
+	const fallbackLanguage = sourceLanguageTag;
+
+	const language = preferredLanguage || acceptLanguage || fallbackLanguage;
+
+	console.log(language);
 
 	return await resolve(event, {
 		transformPageChunk({ html, done }) {
 			if (done) {
-				return html.replace('%lang%', lang);
+				return html.replace('%lang%', language);
 			}
 		}
 	});
