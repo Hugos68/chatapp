5eff9cf4e987e407a7beb9a3e527deafa36d72ca
hugos68 <hugokorteapple@gmail.com>
2023-12-26 00:07:14 +0100

    Added marked, removed mdsvex

---
+++ src/routes/(app)/(protected)/create/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { micromark } from 'micromark';
+
+	let value: string;
+</script>
+
+<textarea class="input" bind:value />
+
+<article class="prose">
+	{@html micromark(value)}
+</article>
+
+<style>
+</style>
